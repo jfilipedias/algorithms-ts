a0858893716308a22c9bae9e7a663b41528029d7
Filipe <filipediascontato@gmail.com>
2022-01-27 12:41:27 -0300

    Implements selection sort

---
+++ src/algorithms/sorting/selectionSort/index.ts
@@ -0,0 +1,15 @@
+import { swap } from "..";
+
+function selectionSort(list: number[]) {
+  for (let i = 0; i < list.length - 1; i += 1) {
+    let minValueIndex = i;
+
+    for (let j = minValueIndex; j < list.length; j += 1) {
+      if (list[j] < list[minValueIndex]) minValueIndex = j;
+
+      if (list[i] > list[minValueIndex]) swap(list, i, minValueIndex);
+    }
+  }
+}
+
+export default selectionSort;
