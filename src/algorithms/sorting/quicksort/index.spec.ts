93c89f8c2d156657df056add6f12983480fff466
Filipe <filipediascontato@gmail.com>
2022-01-27 10:48:51 -0300

    Ajust quicksort test

--- src/algorithms/sorting/quicksort/index.spec.ts
+++ src/algorithms/sorting/quicksort/index.spec.ts
@@ -1,10 +1,10 @@
 import quicksort from ".";
 
 describe("Tests quicksort algorithm", () => {
-  it("Should be able to order a list", () => {
-    const list = [2, 5, 10, 3, 0];
+  it("Should be able to sort a number list", () => {
+    const list = [2, 12, 5, 10, 3, 0, 1];
     quicksort(list);
 
-    expect(list).toStrictEqual([0, 2, 3, 5, 10]);
+    expect(list).toStrictEqual([0, 1, 2, 3, 5, 10, 12]);
   });
 });
