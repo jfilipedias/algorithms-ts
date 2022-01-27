0cd79b3b5281aa988bcee0e00e0ea8add2c57f1c
Filipe <filipediascontato@gmail.com>
2022-01-27 13:32:56 -0300

    Implements bubble sort algorithm

---
+++ src/algorithms/sorting/bubbleSort/index.ts
@@ -0,0 +1,13 @@
+import { swap } from "..";
+
+function bubbleSort(list: number[]) {
+  for (let i = 0; i < list.length - 1; i += 1) {
+    for (let j = 0; j < list.length - 1; j += 1) {
+      if (list[j] > list[j + 1]) {
+        swap(list, j, j + 1);
+      }
+    }
+  }
+}
+
+export default bubbleSort;
