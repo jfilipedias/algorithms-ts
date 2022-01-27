8744b9a04083e7548a51406bf02a0b8bf4753b85
Filipe <filipediascontato@gmail.com>
2022-01-27 10:35:14 -0300

    Implements quicksort algorithm

---
+++ src/algorithms/sorting/quicksort/index.ts
@@ -0,0 +1,35 @@
+function quicksort(list: number[], start = 0, end = -1) {
+  if (list.length === 1) return;
+
+  if (end === -1) end = list.length - 1;
+
+  if (start < end) {
+    const pivotIndex = partition(list, start, end);
+    quicksort(list, start, pivotIndex - 1);
+    quicksort(list, pivotIndex + 1, end);
+  }
+}
+
+function partition(list: number[], start: number, end: number): number {
+  let index = start;
+  const pivotValue = list[end];
+
+  for (let i = start; i < end; i += 1) {
+    if (list[i] <= pivotValue) {
+      swap(list, i, index);
+      index += 1;
+    }
+  }
+
+  swap(list, index, end);
+
+  return index;
+}
+
+function swap(list: number[], indexA: number, indexB: number) {
+  const temp = list[indexA];
+  list[indexA] = list[indexB];
+  list[indexB] = temp;
+}
+
+export default quicksort;
