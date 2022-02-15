95fc394a233d63dc595f5c3254c08317e8976856
Filipe <filipediascontato@gmail.com>
2022-02-15 15:04:25 -0300

    Implements set and insert method in the linked list

--- src/dataStructure/linkedList/index.ts
+++ src/dataStructure/linkedList/index.ts
@@ -14,23 +14,52 @@ class LinkedList<Type> {
       throw new Error("Index out of range.");
     }
 
-    let current = this.head;
+    let node = this.head;
     for (let i = 0; i < index; i += 1) {
-      current = current.next;
+      node = node.next;
     }
 
-    return current.data;
+    return node.data;
+  }
+
+  public set(index: number, data: Type): void {
+    if (index < 0 || index >= this.length) {
+      throw new Error("Index out of range.");
+    }
+
+    let node = this.head;
+    for (let i = 0; i < index; i += 1) {
+      node = node.next;
+    }
+
+    node.data = data;
+  }
+
+  public insert(index: number, data: Type): void {
+    if (index < 0 || index > this.length) {
+      throw new Error("Index out of range.");
+    }
+
+    if (index === 0) {
+      this.head = new Node<Type>(data, this.head);
+    } else {
+      let node = this.head;
+      for (let i = 0; i < index - 1; i += 1) {
+        node = node.next;
+      }
+      node.next = new Node<Type>(data, node.next);
+    }
   }
 
   public append(data: Type): void {
     if (!this.head) {
       this.head = new Node<Type>(data);
     } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
+      let node = this.head;
+      while (node.next) {
+        node = node.next;
       }
-      current.next = new Node<Type>(data);
+      node.next = new Node<Type>(data);
     }
 
     this.length += 1;
