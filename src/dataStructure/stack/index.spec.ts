7efd159477d6e6e8a4a97deb308a2393e5ac65e5
Filipe <filipediascontato@gmail.com>
2022-02-16 16:51:28 -0300

    Implements test to queue

---
+++ src/dataStructure/stack/index.spec.ts
@@ -0,0 +1,53 @@
+import { Stack } from ".";
+
+describe("Test stack data structure", () => {
+  it("Should be able to create a empty stack", () => {
+    const stack = new Stack<number>();
+
+    expect(stack.isEmpty()).toBe(true);
+    expect(stack.size()).toBe(0);
+    expect(stack.toString()).toBe("");
+    expect(() => stack.peek()).toThrow(
+      "Can not get element from an empty stack."
+    );
+  });
+
+  it("Should be able to push data", () => {
+    const stack = new Stack<number>();
+    stack.push(10);
+    expect(stack.size()).toBe(1);
+
+    stack.push(-3);
+    expect(stack.size()).toBe(2);
+    expect(stack.toString()).toBe("-3, 10");
+  });
+
+  it("Should be able to pop data", () => {
+    const stack = new Stack<number>();
+    stack.push(10);
+    stack.push(-3);
+    stack.push(0);
+
+    const element = stack.pop();
+    expect(element).toEqual(0);
+    expect(stack.size()).toBe(2);
+    expect(stack.peek()).toEqual(-3);
+    expect(stack.toString()).toBe("-3, 10");
+  });
+
+  it("Should not be able to pop an empty stack", () => {
+    const stack = new Stack<number>();
+    expect(() => stack.pop()).toThrow(
+      "Can not pop element from an empty stack."
+    );
+  });
+
+  it("Should be able to peek data", () => {
+    const stack = new Stack<number>();
+    stack.push(10);
+    stack.push(-3);
+
+    expect(stack.peek()).toEqual(-3);
+    expect(stack.toString()).toBe("-3, 10");
+  });
+});
