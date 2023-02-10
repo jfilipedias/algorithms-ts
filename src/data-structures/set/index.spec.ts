daa7e5b846250f65c256a5869e4bd663a534761e
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-02-10 11:20:19 -0300

    Implements set operations

--- src/data-structures/set/index.spec.ts
+++ src/data-structures/set/index.spec.ts
@@ -3,7 +3,7 @@ import { Set } from '.'
 
 describe('Test set data structure', () => {
   it('Should be able to create a empty set', () => {
-    const set = new Set()
+    const set = new Set<number>()
 
     expect(set.size()).toEqual(0)
     expect(set.isEmpty()).toBeTruthy()
@@ -11,7 +11,7 @@ describe('Test set data structure', () => {
   })
 
   it('Should be able to add an element', () => {
-    const set = new Set()
+    const set = new Set<number>()
 
     expect(set.add(1)).toBeTruthy()
     expect(set.isEmpty()).toBeFalsy()
@@ -23,7 +23,7 @@ describe('Test set data structure', () => {
   })
 
   it('Should not be able to add the same element', () => {
-    const set = new Set()
+    const set = new Set<number>()
 
     expect(set.add(1)).toBeTruthy()
     expect(set.add(1)).toBeFalsy()
@@ -31,7 +31,7 @@ describe('Test set data structure', () => {
   })
 
   it('Should be able to remove an element', () => {
-    const set = new Set()
+    const set = new Set<number>()
 
     set.add(1)
     set.add(2)
@@ -42,7 +42,7 @@ describe('Test set data structure', () => {
   })
 
   it('Should not be able to remove a non-existing element', () => {
-    const set = new Set()
+    const set = new Set<number>()
 
     expect(set.remove(1)).toBeFalsy()
 
@@ -52,12 +52,12 @@ describe('Test set data structure', () => {
   })
 
   it('Should be able to join two sets', () => {
-    const setA = new Set()
+    const setA = new Set<number>()
     setA.add(1)
     setA.add(2)
     setA.add(3)
 
-    const setB = new Set()
+    const setB = new Set<number>()
     setB.add(2)
     setB.add(3)
     setB.add(4)
@@ -68,8 +68,56 @@ describe('Test set data structure', () => {
     expect(unionSet.values()).toEqual([1, 2, 3, 4, 5])
   })
 
+  it('Should be able to take the intersection of two sets', () => {
+    const setA = new Set<number>()
+    setA.add(1)
+    setA.add(2)
+    setA.add(3)
+
+    const setB = new Set<number>()
+    setB.add(2)
+    setB.add(3)
+    setB.add(4)
+    setB.add(5)
+
+    const unionSet = setA.intersection(setB)
+
+    expect(unionSet.values()).toEqual([2, 3])
+  })
+
+  it('Should be able to take the diference of two sets', () => {
+    const setA = new Set<number>()
+    setA.add(1)
+    setA.add(2)
+    setA.add(3)
+    setA.add(4)
+
+    const setB = new Set<number>()
+    setB.add(3)
+    setB.add(4)
+    setB.add(5)
+
+    const unionSet = setA.difference(setB)
+
+    expect(unionSet.values()).toEqual([1, 2])
+  })
+
+  it('Should be able check if is a subset', () => {
+    const setA = new Set<number>()
+    setA.add(2)
+    setA.add(3)
+
+    const setB = new Set<number>()
+    setB.add(1)
+    setB.add(2)
+    setB.add(3)
+
+    expect(setA.isSubsetOf(setB)).toBeTruthy()
+    expect(setB.isSubsetOf(setA)).toBeFalsy()
+  })
+
   it('Should not be able to clear a set', () => {
-    const set = new Set()
+    const set = new Set<number>()
 
     set.add(1)
     set.add(2)
--- src/data-structures/set/index.ts
+++ src/data-structures/set/index.ts
@@ -1,39 +1,11 @@
-export class Set {
-  constructor(private items: { [key: PropertyKey]: PropertyKey } = {}) {}
+export class Set<T> {
+  constructor(private items: { [key: PropertyKey]: T } = {}) {}
 
-  public has(element: PropertyKey) {
-    return Object.prototype.hasOwnProperty.call(this.items, element)
-  }
-
-  public add(element: PropertyKey) {
-    if (this.has(element)) {
-      return false
-    }
-
-    this.items[element] = element
-    return true
-  }
-
-  public remove(element: PropertyKey) {
-    if (this.isEmpty() || !this.has(element)) {
-      return false
-    }
-
-    delete this.items[element]
-    return true
-  }
-
-  public union(otherSet: Set) {
-    const unionSet = new Set()
-
-    Object.values(this.values()).forEach((value) => unionSet.add(value))
-    Object.values(otherSet.values()).forEach((value) => unionSet.add(value))
-
-    return unionSet
-  }
-
-  public values() {
-    return Object.values(this.items)
+  public has(element: T) {
+    return Object.prototype.hasOwnProperty.call(
+      this.items,
+      element as PropertyKey,
+    )
   }
 
   public size() {
@@ -46,5 +18,82 @@ export class Set {
 
   public clear() {
     this.items = {}
+  }
+
+  public values() {
+    return Object.values(this.items)
+  }
+
+  public add(element: T) {
+    if (this.has(element)) {
+      return false
+    }
+
+    this.items[element as PropertyKey] = element
+    return true
+  }
+
+  public remove(element: T) {
+    if (this.isEmpty() || !this.has(element)) {
+      return false
+    }
+
+    delete this.items[element as PropertyKey]
+    return true
+  }
+
+  public union(otherSet: Set<T>) {
+    const unionSet = new Set<T>()
+
+    this.values().forEach((value) => unionSet.add(value))
+    otherSet.values().forEach((value) => unionSet.add(value))
+
+    return unionSet
+  }
+
+  public intersection(otherSet: Set<T>) {
+    const intersectionSet = new Set<T>()
+
+    this.values().forEach((value) => {
+      if (otherSet.has(value)) {
+        intersectionSet.add(value)
+      }
+    })
+
+    return intersectionSet
+  }
+
+  public difference(otherSet: Set<T>) {
+    const differenceSet = new Set<T>()
+
+    this.values().forEach((value) => {
+      if (!otherSet.has(value)) {
+        differenceSet.add(value)
+      }
+    })
+
+    return differenceSet
+  }
+
+  public isSubsetOf(otherSet: Set<T>) {
+    const currentSetValues = this.values()
+    const otherSetValue = otherSet.values()
+
+    if (currentSetValues.length > otherSetValue.length) {
+      return false
+    }
+
+    let isSubset = true
+
+    currentSetValues.every((value) => {
+      if (!otherSet.has(value)) {
+        isSubset = false
+        return false
+      }
+
+      return true
+    })
+
+    return isSubset
   }
 }
